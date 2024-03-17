82302d42912fd6db4bf21f69f10faf973d36858f
sp-sofi <62249700+sp-sofi@users.noreply.github.com>
2024-03-17 14:53:29 +0200

    added unittest test_read_from_file for empty files and empty.txt

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -4,8 +4,13 @@ from app.io.input import read_from_file, read_from_file_pandas
 
 class TestInputFunctions(unittest.TestCase):
     def test_read_from_file(self):
-        file_content = read_from_file("../../test_data.txt")
+        file_content = read_from_file("../../test_data/test_data.txt")
         self.assertEqual(file_content, "This is a test text")
+
+    def test_read_from_file_empty(self):
+        file_content = read_from_file("../../test_data/empty.txt")
+        self.assertEqual(file_content, "")
+
 
     def test_read_from_file_pandas(self):
         dataframe = read_from_file_pandas("../../data.csv")
